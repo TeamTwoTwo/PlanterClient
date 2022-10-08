191504db92b735421cba49357b025ee98ec4f339
2Junsu <ijh1205@naver.com>
2022-10-08 18:10:22 +0900

    Update: width, borderRadius 추가

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {TouchableOpacity, Text, StyleSheet, Dimensions} from 'react-native';
 import {color} from '../../utils/color';
 
-const width = Dimensions.get('window').width;
-
 interface Props {
   backgroundColor: string;
   text: string;
   disabled?: boolean;
+  width?: number | string;
+  borderRadius?: number;
   onPress: () => void;
 }
 
@@ -15,12 +15,14 @@ const CustomButton = ({
   backgroundColor,
   text,
   disabled = false,
+  width = Dimensions.get('window').width,
+  borderRadius = 0,
   onPress,
 }: Props) => {
   return (
     <TouchableOpacity
       disabled={disabled}
-      style={dstyles(backgroundColor, disabled).button}
+      style={dstyles(backgroundColor, disabled, width, borderRadius).button}
       activeOpacity={1}
       onPress={onPress}>
       <Text style={styles.buttonText}>{text}</Text>
@@ -28,7 +30,12 @@ const CustomButton = ({
   );
 };
 
-const dstyles = (backgroundColor: string, disabled: boolean) =>
+const dstyles = (
+  backgroundColor: string,
+  disabled: boolean,
+  width: number | string,
+  borderRadius: number,
+) =>
   StyleSheet.create({
     button: {
       backgroundColor: disabled ? color.gray_03 : backgroundColor,
@@ -36,6 +43,7 @@ const dstyles = (backgroundColor: string, disabled: boolean) =>
       alignItems: 'center',
       justifyContent: 'center',
       width,
+      borderRadius,
     },
   });
 
