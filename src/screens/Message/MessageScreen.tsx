281591ad239fa7f4e3a267d3e47154b8d2091700
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2022-11-08 00:48:20 +0900

    Feat: 쪽지함 리스트 조회 api 구현

--- src/screens/Message/MessageScreen.tsx
+++ src/screens/Message/MessageScreen.tsx
@@ -1,33 +1,74 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, Pressable, FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {color, Typography} from '../../utils/utils';
+import {color, Typography, url} from '../../utils/utils';
 import MessageItem from '../../components/Message/MessageItem';
 import {useNavigation} from '@react-navigation/native';
 import {MainTabNavigationProp} from '../../screens/MainTab';
 import MatchingHeader from '../../components/matching/MatchingHeader';
+import axios from 'axios';
+import {getData} from '../../utils/AsyncStorage';
+
+interface messageData {
+  plantManagereId: number;
+  profileImg: string;
+  name: string;
+  category: number;
+  contents: string;
+  sentAt: string;
+  isUnread: boolean;
+}
 
 const MessageScreen = () => {
-  const [state, setState] = useState<number>(1);
+  const [messageList, setMessageList] = useState<messageData[]>();
   const navigation = useNavigation<MainTabNavigationProp>();
+
+  useEffect(() => {
+    getData('auth').then(auth => {
+      axios
+        .get(url.dev + 'messages', {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        })
+        .then(res => {
+          console.log(res.data);
+          setMessageList(res.data.result);
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    });
+  }, []);
 
   return (
     <SafeAreaView style={styles.safe}>
       <MatchingHeader title="쪽지함" />
-      {state === 0 ? (
+      {messageList ? (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={messageList}
+          renderItem={({item}: {item: messageData}) => (
+            <MessageItem
+              onPress={() => {
+                navigation.navigate('MessageDetailScreen');
+              }}
+              name={item.name}
+              contents={item.contents}
+              profileImg={item.profileImg}
+              sentAt={item.sentAt}
+              isUnread={item.isUnread}
+              category={item.category}
+            />
+          )}
+          keyExtractor={(item: messageData) => item.plantManagereId}
+        />
+      ) : (
         <View style={styles.contentWrap}>
           <View style={styles.img} />
           <Text stylee={[Typography.body1, {color: color.blueGray_06}]}>
             받은 쪽지가 없습니다.
           </Text>
-        </View>
-      ) : (
-        <View>
-          <MessageItem
-            onPress={() => {
-              navigation.navigate('MessageDetailScreen');
-            }}
-          />
         </View>
       )}
     </SafeAreaView>
