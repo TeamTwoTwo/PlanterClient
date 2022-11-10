5945c833ae6ee1322c24089d6db56ca3f5d1f21c
2Junsu <ijh1205@naver.com>
2022-11-10 14:20:41 +0900

    Feat: 스플래시 스크린 구현

--- android/app/src/main/java/com/planter/MainActivity.java
+++ android/app/src/main/java/com/planter/MainActivity.java
@@ -3,9 +3,16 @@ package com.planter;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
-
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {RecoilRoot} from 'recoil';
 import Navigator from './screens/Navigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
+
   return (
     <RecoilRoot>
       <Navigator />
