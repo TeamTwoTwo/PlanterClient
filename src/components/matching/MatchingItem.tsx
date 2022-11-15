a8d57b3528dad713c3f2ddac9adab824684229c4
2Junsu <ijh1205@naver.com>
2022-11-16 01:22:24 +0900

    Design: 매칭 카드 shadow 수정

--- src/components/matching/MatchingItem.tsx
+++ src/components/matching/MatchingItem.tsx
@@ -6,6 +6,7 @@ import Star from '../../assets/icon/ic-star.svg';
 import Expert from '../../assets/icon/ic-expert-badge.svg';
 import Flower from '../../assets/icon/ic-flower-badge.svg';
 import Care from '../../assets/icon/ic-care-badge.svg';
+import {Shadow} from 'react-native-shadow-2';
 
 interface PropTypes {
   id: number;
@@ -35,77 +36,81 @@ const MatchingItem = ({
   onPress,
 }: PropTypes) => {
   return (
-    <Pressable style={[styles.box, styles.shadow]} onPress={onPress}>
-      <View>
-        <Image style={styles.profile} source={{uri: profileImg}} />
-      </View>
-      <View style={styles.content}>
-        <View style={styles.line}>
-          <Text style={[Typography.subtitle3, styles.name]}>
-            {name.length > maxlimit
-              ? name.slice(0, maxlimit + 1) + ' ...'
-              : name}
-          </Text>
-          {category === 0 && (
-            <View style={styles.line}>
-              <Butler />
-              <Text style={styles.job}>식물 집사</Text>
-            </View>
-          )}
-          {category === 1 && (
-            <View style={styles.line}>
-              <Flower />
-              <Text style={styles.job}>꽃집</Text>
-            </View>
-          )}
-          {category === 2 && (
-            <View style={styles.line}>
-              <Expert />
-              <Text style={styles.job}>식물 전문가</Text>
-            </View>
-          )}
-          {category === 3 && (
-            <View style={styles.line}>
-              <Care />
-              <Text style={styles.job}>식물케어 서비스</Text>
-            </View>
-          )}
+    <Shadow startColor="rgba(190,202,223,0.1)" offset={[0, 0]}>
+      <Pressable style={styles.box} onPress={onPress}>
+        <View>
+          <Image style={styles.profile} source={{uri: profileImg}} />
         </View>
-        <View style={styles.line}>
-          {isPhoto ? (
-            <Text style={[Typography.caption1, styles.info]}>{distance}km</Text>
-          ) : (
-            <Text style={[Typography.caption1, styles.info]}>
-              {distance}km ·{' '}
+        <View style={styles.content}>
+          <View style={styles.line}>
+            <Text style={[Typography.subtitle3, styles.name]}>
+              {name.length > maxlimit
+                ? name.slice(0, maxlimit + 1) + ' ...'
+                : name}
             </Text>
-          )}
-          {isPhoto && (
+            {category === 0 && (
+              <View style={styles.line}>
+                <Butler />
+                <Text style={styles.job}>식물 집사</Text>
+              </View>
+            )}
+            {category === 1 && (
+              <View style={styles.line}>
+                <Flower />
+                <Text style={styles.job}>꽃집</Text>
+              </View>
+            )}
+            {category === 2 && (
+              <View style={styles.line}>
+                <Expert />
+                <Text style={styles.job}>식물 전문가</Text>
+              </View>
+            )}
+            {category === 3 && (
+              <View style={styles.line}>
+                <Care />
+                <Text style={styles.job}>식물케어 서비스</Text>
+              </View>
+            )}
+          </View>
+          <View style={styles.line}>
+            {isPhoto ? (
+              <Text style={[Typography.caption1, styles.info]}>
+                {distance}km
+              </Text>
+            ) : (
+              <Text style={[Typography.caption1, styles.info]}>
+                {distance}km ·{' '}
+              </Text>
+            )}
+            {isPhoto && (
+              <Text style={[Typography.caption1, styles.info]}>
+                {' '}
+                · 사진 제공 ·{' '}
+              </Text>
+            )}
+            <View>
+              <Star fill="#ffc42c" />
+            </View>
             <Text style={[Typography.caption1, styles.info]}>
               {' '}
-              · 사진 제공 ·{' '}
+              {rate.toFixed(1)}
             </Text>
-          )}
-          <View>
-            <Star fill="#ffc42c" />
           </View>
-          <Text style={[Typography.caption1, styles.info]}>
-            {' '}
-            {rate.toFixed(1)}
-          </Text>
+          <View style={styles.bodyLine}>
+            <Text style={[Typography.body2, styles.bodyText]} numberOfLines={2}>
+              {description}
+            </Text>
+          </View>
+          <View style={styles.moneyLine}>
+            <Text style={[Typography.subtitle4, {color: color.blueGray_06}]}>
+              {minPrice.toLocaleString()}원~
+            </Text>
+            <Text style={[Typography.body2, styles.day]}>/ 1일</Text>
+          </View>
         </View>
-        <View style={styles.bodyLine}>
-          <Text style={[Typography.body2, styles.bodyText]} numberOfLines={2}>
-            {description}
-          </Text>
-        </View>
-        <View style={styles.moneyLine}>
-          <Text style={[Typography.subtitle4, {color: color.blueGray_06}]}>
-            {minPrice.toLocaleString()}원~
-          </Text>
-          <Text style={[Typography.body2, styles.day]}>/ 1일</Text>
-        </View>
-      </View>
-    </Pressable>
+      </Pressable>
+    </Shadow>
   );
 };
 
@@ -121,22 +126,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginLeft: 22,
     marginRight: 18,
-  },
-  shadow: {
-    ...Platform.select({
-      ios: {
-        shadowColor: 'rgb(190,202,223)',
-        shadowOffset: {
-          width: 0,
-          height: 0,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 15,
-      },
-      android: {
-        elevation: 1,
-      },
-    }),
   },
   profile: {
     width: 80,
